a3d9c7da4f743d1aa9fa9cf14e9deba3a472322e
cengo12 <57234794+cengo12@users.noreply.github.com>
2023-03-02 23:04:07 +0300

    remove devtools

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const dbmanager = require('./database/dbmanager.js')
-const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -34,6 +33,9 @@ app.on('ready', () => {
   ipcMain.handle('getMembers', ()=>dbmanager.getMembers());
   createWindow();
 })
+
+
+
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
